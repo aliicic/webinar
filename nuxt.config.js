3492d6b61562a8b605be715e5ea0a246261913fd
aliicic <aliaghakhani911@gmail.com>
2022-09-07 16:00:42 +0430

    nuxt config export module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
 
@@ -36,91 +36,13 @@ export default {
         content: process.env.npm_package_description || "",
       },
     ],
-    link: [
-      // {
-      //   rel: "icon",
-      //   type: "image/x-icon",
-      //   href: "/favicon.png"
-      // },
-      // // {
-      // //   rel: "stylesheet",
-      // //   href: "/css/bootstrap.min.css"
-      // // },
-      // {
-      //   rel: "stylesheet",
-      //   href:
-      //     "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      // },
-      // // {
-      // //   rel: "stylesheet",
-      // //   href: "/css/slick/slick.css"
-      // // },
-      // // {
-      // //   rel: "stylesheet",
-      // //   href: "/css/slick/slick-theme.css"
-      // // },
-      // // {
-      // //   rel: "stylesheet",
-      // //   href: "/css/vue-slick-carousel.css"
-      // // },
-      // { rel: "stylesheet", href: "/css/stl.css" },
-      // // { rel: "stylesheet", href: "/css/mml/mml.css" },
-      // {
-      //   rel: "stylesheet",
-      //   href: "/css/style.css"
-      // },
-      // {
-      //   rel: "stylesheet",
-      //   href: "/css/responsive.css"
-      // }
-      // {
-      //   rel: "stylesheet",
-      //   href: "/css/loading.css"
-      // },
-      // {
-      //   rel: "stylesheet",
-      //   href: "/css/animate.min.css"
-      // },
-      // {
-      //   rel: "stylesheet",
-      //   href: "/css/swiper.css"
-      // }
-    ],
-    script: [
-      // {
-      //   src: "/js/jquery.min.js",
-      //   defer: true
-      //   // body: true
-      // },
-      // {
-      //   src: "/js/bootstrap.min.js",
-      //   defer: true,
-      //   body: true
-      // },
-      // {
-      //   src: "/js/popper.min.js",
-      //   defer: true,
-      //   body: true
-      // },
-      // {
-      //   src: "/js/slick/slick.min.js",
-      //   defer: true,
-      //   body: true
-      // },
-      // { src: "/js/mml/mml.js", defer: true },
-      // {
-      //   src: "/js/main.js",
-      //   defer: true,
-      //   body: true
-      // }
-    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "~/plugins/socket.io.js" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -144,7 +66,7 @@ export default {
       {
         // At least one entry is required
         name: "home",
-        url: "http://localhost:3001/webinars",
+        url: "http://localhost:3000/webinars",
         default: true,
         // vuex: {
         //   /* see section below */
@@ -153,6 +75,11 @@ export default {
       },
     ],
   },
+  // env: {
+  //   WS_URL: process.env.WS_URL || "http://localhost:3000",
+  //   HOST: process.env.HOST || "localhost",
+  //   PORT: process.env.PORT || 3000,
+  // },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
@@ -170,9 +97,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    postcss: null,
+    postcss: false,
   },
-  serverMiddleware: {
-    "/api": "~/api",
-  },
+  // serverMiddleware: {
+  //   "/api": "~/api",
+  // },
 };
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const port = process.env.PORT || 3000;
 const isProd = process.env.NODE_ENV === "production";
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const { AllRoutes } = require("./router/router");
-const { initialSocket } = require("./utils/initSocket");
-const { socketHandler } = require("./socket.io");
-
- const http = require("http");
+const http = require("http");
 const app = require("express")();
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
-
 const { Nuxt, Builder } = require("nuxt");
 // We instantiate Nuxt with the options
-const config = require("../nuxt.config.js");
+const config = require("./nuxt.config.js");
 config.dev = !isProd;
 
 const nuxt = new Nuxt(config);
@@ -28,11 +19,14 @@ if (config.dev) {
 }
 app.use(nuxt.render);
 
-
-io.on('connection', () => {
-  console.log('user comming')
-})
-
-
+// Listen the server
 server.listen(port, "0.0.0.0");
-console.log("Server listening on localhost:" + port);
+console.log("Server listening on localhost:" + port); // eslint-disable-line no-console
+
+// Socket.ionn
+// Socket.io
+const messages = [];
+io.on("connection", (socket) => {
+  console.log("uh yes");
+  socket.emit("roomList", [{ name: "room1" }]);
+});
